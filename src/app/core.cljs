33bb753c0df1f79f79c35cbdf9920c30587fafed
amjil <amjilt@aliyun.com>
2021-10-12 22:24:30 +0800

    add smooth-blink-view

--- src/app/core.cljs
+++ src/app/core.cljs
@@ -8,44 +8,49 @@
    [cljs-bean.core :refer [->clj ->js]]
    [goog.crypt.base64 :as b64]
    [app.font.base :as font]
-   [app.text.index :as text]))
+   [app.text.index :as text]
+   ["react-native-smooth-blink-view" :default blinkview]))
 
 (defn root-comp []
   [safe-area/safe-area-view
    [rn/view {:style {:flexDirection "column"}}
-    [rn/text {:style {:backgroundColor "green"}} "Hello CLojure! from CLJS "]]])
+    [rn/text {:style {:backgroundColor "green"}} "Hello CLojure! from CLJS "]
+    [:> blinkview {"useNativeDriver" false}
+     [rn/view {:style {:position :absolute :top 40 :left 86}}
+      [:> svg/Svg {:width 24 :height 6}
+       [:> svg/Rect {:x "0" :y "0" :width 48 :height 2 :fill "black"}]]]]
     ;; [:> svg/Svg {:width "130" :height "130" :fill "blue" :stroke "red" :color "green" :viewBox "-16 -16 544 544" :style {:transform [{:scaleX -1}]}}
-    ; (if (font/get-font :white)
-    ;   [rn/view {:style {:margin 10
-    ;                     :flex 1
-    ;                     :backgroundColor :white}}
-    ;
-    ;    [:> svg/Svg {:width  "100%"
-    ;                 :height "530"
-    ;                 :fill   "blue"
-    ;                 :color  "green"
-    ;                 :top 0
-    ;                 :left 0
-    ;                 :position  "relative"}
-    ;     ; [:> svg/Rect {:x      "0"
-    ;     ;               :y      "0"
-    ;     ;               :width  "28"
-    ;     ;               :height 520
-    ;     ;               :fill   "#318FFE"}]
-    ;     (map-indexed
-    ;      (fn [idx item]
-    ;        (map-indexed
-    ;         (fn [i run]
-    ;           (if-not (empty? (:svg run))
-    ;             (let [x (if (= idx 0) "3" (str (* idx 40)))]
-    ;               [:> svg/Path {:d        (:svg run)
-    ;                             :x        x
-    ;                             :y        (str (:y run))
-    ;                             :rotation "90"
-    ;                             :key      (str idx "-" i)}])))
-    ;         item))
-    ;      (text/text-component 25.5 520 font/mlongstr))]])
-    
+    (if (font/get-font :white)
+      [rn/view {:style {:margin 10
+                        :flex 1
+                        :backgroundColor :white}}
+
+       [:> svg/Svg {:width  "100%"
+                    :height "530"
+                    :fill   "blue"
+                    :color  "green"
+                    :top 0
+                    :left 0
+                    :position  "relative"}
+        ; [:> svg/Rect {:x      "0"
+        ;               :y      "0"
+        ;               :width  "28"
+        ;               :height 520
+        ;               :fill   "#318FFE"}]
+        (map-indexed
+         (fn [idx item]
+           (map-indexed
+            (fn [i run]
+              (if-not (empty? (:svg run))
+                (let [x (if (= idx 0) "3" (str (* idx 40)))]
+                  [:> svg/Path {:d        (:svg run)
+                                :x        x
+                                :y        (str (:y run))
+                                :rotation "90"
+                                :key      (str idx "-" i)}])))
+            item))
+         (text/text-component 25.5 520 font/mlongstr))]])]])
+
 
 
 (defn init []
