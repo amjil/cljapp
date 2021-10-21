bbae1d1008cbc9d3b59b6a54d7508428c197cc9d
amjil <amjil@protonmail.com>
2021-10-21 20:13:32 +0800

    add keyboard folder

--- src/app/core.cljs
+++ src/app/core.cljs
@@ -2,6 +2,8 @@
   (:require
    [steroid.rn.core :as rn]
    [steroid.rn.navigation.safe-area :as safe-area]
+   [steroid.rn.components.touchable :as touchable]
+   [steroid.rn.components.ui :as rn-ui]
    [reagent.core :as reagent]
    ["react-native-svg" :as svg]
    [applied-science.js-interop :as j]
@@ -9,40 +11,37 @@
    [goog.crypt.base64 :as b64]
    [app.font.base :as font]
    [app.text.index :as text]
+   [clojure.string :as str]
    ["react-native-smooth-blink-view" :default blinkview]))
 
 (defn root-comp []
-  (let [w (reagent/atom 200)]
-    [safe-area/safe-area-view
-     [rn/view {:style {:flexDirection "column"
-                       :height "100%"
-                       :width "100%"}
-               :on-layout #(do (js/console.log "on layout !!!!")
-                               (reset! w (-> %
-                                           (j/get :nativeEvent)
-                                           (j/get :layout)
-                                           (j/get :height)))
-                               (js/console.log
-                                 (-> %
-                                   (j/get  :nativeEvent)
-                                   (j/get :layout)
-                                   (j/get :height))))}
-      [rn/text {:style {:backgroundColor "green"}} "Hello CLojure! from CLJS "]
-      ; [:> blinkview {"useNativeDriver" false}
-      ;  [rn/view {:style {:position :absolute :top 40 :left 86}}
-      ;   [:> svg/Svg {:width 24 :height 6}
-      ;    [:> svg/Rect {:x "0" :y "0" :width 48 :height 2 :fill "black"}]]]]
-      ; [rn/view {:style {:position :absolute :top 140 :left 81}}
-      ;  [:> svg/Svg {:width 45 :height 20}
-      ;   [:> svg/Circle {:cx "5" :cy "5" :r "5" :fill "black"}]
-      ;   [:> svg/Rect {:x "10" :y "4.5" :width 25 :height 2 :fill "black"}]]]
-      ; [rn/view {:style {:position :absolute :top 150 :left 81}}
-      ;  [:> svg/Svg {:width 45 :height 20}
-      ;   [:> svg/Circle {:cx "40" :cy "5" :r "5" :fill "black"}]
-      ;   [:> svg/Rect {:x "10" :y "4.5" :width 25 :height 2 :fill "black"}]]]
-      ; (text/text-area (text/text-component 0 500 :white 24 font/mlongstr) {:line-height 40 :fill "blue" :color "black" :top 0 :left 0})]])
-      ; (text/flat-list-text {:width 26 :fill "black" :color "black"} (text/text-component 0 @w :white 18 font/mlongstr))
-      (text/flat-list-text {:width 26 :fill "black" :color "black" :height 500 :font :white :font-size 18} font/mlongstr)]]))
+  [safe-area/safe-area-view
+   [rn/view {:style {:flexDirection "column"
+                     :height "100%"
+                     :width "100%"}}
+    [rn/text {:style {:backgroundColor "green"}} "Hello CLojure! from CLJS "]
+    ; [:> blinkview {"useNativeDriver" false}
+    ;  [rn/view {:style {:position :absolute :top 40 :left 86}}
+    ;   [:> svg/Svg {:width 24 :height 6}
+    ;    [:> svg/Rect {:x "0" :y "0" :width 48 :height 2 :fill "black"}]]]]
+    ; [rn/view {:style {:position :absolute :top 140 :left 81}}
+    ;  [:> svg/Svg {:width 45 :height 20}
+    ;   [:> svg/Circle {:cx "5" :cy "5" :r "5" :fill "black"}]
+    ;   [:> svg/Rect {:x "10" :y "4.5" :width 25 :height 2 :fill "black"}]]]
+    ; [rn/view {:style {:position :absolute :top 150 :left 81}}
+    ;  [:> svg/Svg {:width 45 :height 20}
+    ;   [:> svg/Circle {:cx "40" :cy "5" :r "5" :fill "black"}]
+    ;   [:> svg/Rect {:x "10" :y "4.5" :width 25 :height 2 :fill "black"}]]]
+
+    ; (text/flat-list-text {:width 26 :fill "black" :color "black" :height 500 :font :white :font-size 18} font/mlongstr)]])
+
+    ; [text/text-area {:line-height 26 :fill "black" :color "black"}
+    ;   (text/text-component {:width 26 :height 500 :font :white :font-size 18} font/mlongstr)]]])
+
+    [rn/view {:style {:overflow "scroll"}}
+     [text/text-line {:width 26 :fill "black" :color "black"}
+       (nth (text/text-component {:width 26 :height 5000 :font :white :font-size 18} font/mlongstr) 0)]]]])
+
 
 
 
