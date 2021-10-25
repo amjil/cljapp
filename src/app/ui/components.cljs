ee36911103460f9bdedc73d9ea7f5138e00b9024
amjil <amjilt@aliyun.com>
2021-10-25 21:00:17 +0800

    add text-editor cursor

--- src/app/ui/components.cljs
+++ src/app/ui/components.cljs
@@ -6,7 +6,8 @@
     ["react-native-vector-icons/Ionicons" :default ion-icons-class]
     ["react-native" :as react-native]
     [reagent.core :as reagent]
-    [steroid.rn.navigation.safe-area :as safe-area]))
+    [steroid.rn.navigation.safe-area :as safe-area]
+    [clojure.string :as string]))
 
 (def ion-icons (reagent/adapt-react-class ion-icons-class))
 (def refresh-control-class (reagent/adapt-react-class react-native/RefreshControl))
@@ -18,6 +19,16 @@
       (into [rn/view {:style {:flex             1 :padding-top (.-top insets)
                               :background-color :white}}]
             children)))])
+
+(defn userpic [image size]
+  (let [d size
+        r (/ d 2)]
+    (if (and (not (string/blank? image)) (string/starts-with? image "http"))
+      [rn/image {:style  {:width d :height d :border-radius r}
+                 :source {:uri image}}]
+      [rn/view {:style {:width        d :height d :border-radius r
+                        :border-color :gray
+                        :border-width 1}}])))
 
 (defn refresh-control [props]
   (reagent/as-element [refresh-control-class props]))
--- src/app/ui/views.cljs
+++ src/app/ui/views.cljs
@@ -11,12 +11,20 @@
    [app.ui.components :as ui]
    [steroid.rn.navigation.stack :as stack]
    [re-frame.core :as re-frame]
-   [reagent.core :as reagent]))
+   [reagent.core :as reagent]
+
+   [app.text.index :as text]
+   [app.font.base :as font]
+   [app.components.gesture :as gesture]
+
+   ["react-native-smooth-blink-view" :default blinkview]
+   ["react-native-svg" :as svg]))
 
 
 (when platform/android?
   (status-bar/set-bar-style "dark-content")
   (status-bar/set-translucent true))
+
 
 (def tab-icons
   {"home"    "md-home"
@@ -39,6 +47,39 @@
                     :width "100%"}}
     [rn/text {:style {:backgroundColor "green"}} "Hello CLojure! from CLJS "]])
 
+(defn text-editor [height text]
+  (let [text-svgs (text/text-component {:width 32 :fill "gray" :color "black" :height height :font :white :font-size 24} text)
+        runs (last (last text-svgs))
+        x (reagent/atom nil)
+        y (reagent/atom nil)]
+    (reset! y (:y runs))
+    (reset! x (+ 8 (* 32 (dec (count text-svgs)))))
+    (fn []
+      [gesture/tap-gesture-handler
+       {:onHandlerStateChange #(if (gesture/tap-state-end (j/get % :nativeEvent))
+                                   (let [[ex ey] (text/cursor-location (j/get % :nativeEvent) 32 text-svgs)]
+                                     (js/console.log "x = " ex " y = " ey)
+                                     (reset! x ex)
+                                     (reset! y ey)))}
+       [gesture/pan-gesture-handler {:onGestureEvent #(let [[ex ey] (text/cursor-location (j/get % :nativeEvent) 32 text-svgs)]
+                                                        (js/console.log "x = " ex " y = " ey))}
+        [rn/view {:style {:height "100%" :width "100%"}}
+         [:> blinkview {"useNativeDriver" false}
+          [rn/view {:style {:position :absolute :top (or @y 0) :left (or @x 0)}}
+           [:> svg/Svg {:width 32 :height 2}
+            [:> svg/Rect {:x "0" :y "0" :width 32 :height 2 :fill "black"}]]]]
+         [text/flat-list-text {:width 32 :fill "gray" :color "black" :height height :font :white :font-size 24} text-svgs]]]])))
+
+(defn home []
+  (let [h (reagent/atom nil)]
+    (fn []
+      [rn/view {:style {:height "100%" :width "100%"}
+                :on-layout #(let [height (j/get-in % [:nativeEvent :layout :height])]
+                              (reset! h height))}
+       (when @h
+         [text-editor @h font/mlongstr])])))
+
+
 (defn tabs []
   [bottom-tabs/bottom-tab
    {
@@ -56,10 +97,10 @@
                                  (let [{:keys [color]} (bean/->clj data)
                                        icon (get tab-icons (:name route))]
                                    (reagent/as-element
-                                     [ui/ion-icons {:name icon :color color :size 30}])))})))}
+                                     [ui/ion-icons {:name icon :color color :size 20}])))})))}
 
    [{:name      :home
-     :component component}
+     :component home}
     {:name      :book
      :component component}
     {:name      :edit
@@ -73,4 +114,5 @@
      {:on-ready #(re-frame/dispatch [:initialise-app])}     ;; when navigation initialized and mounted initialize the app
      [stack/stack {}
       [{:name      :main
-        :component tabs}]])]])
+        :component tabs
+        :options {:title "cljapp"}}]])]])
