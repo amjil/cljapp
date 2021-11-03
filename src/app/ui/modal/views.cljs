c4c1048217ad8430f96bb17db55169d3c2c07fa2
amjil <amjil@protonmail.com>
2021-11-03 10:41:25 +0800

    modal padding top bottom

--- src/app/ui/modal/views.cljs
+++ src/app/ui/modal/views.cljs
@@ -38,11 +38,12 @@
                              ;:flexDirection "column"
                              :justifyContent "center"
                              :alignItems "center"
-                             ; :paddingLeft 16
-                             ; :paddingRight 16
+                             :paddingTop 16
+                             :paddingBottom 16
                              ; :borderRightWidth StyleSheet.hairlineWidth,
                              :borderRightWidth 1
-                             :borderColor "#DBDBDB"}
+                             :borderColor "#DBDBDB"
+                             :width 42}
                             (when (= 0 idx)
                               {:borderTopLeftRadius 12
                                :borderBottomLeftRadius 12})
@@ -61,7 +62,7 @@
               :key idx
               :onPress #(do (js/console.log "pressed >>>> " idx)
                             (reset! is-visible false))}
-             [text/text-line {:width 42}
+             [text/text-line {:width 36}
               (first (text/text-component
                        {:width 0 :fill (if (= idx (dec (count items))) "#fa1616" "rgb(0,98,255)")
                         :height 200 :font :white :font-size 18}
