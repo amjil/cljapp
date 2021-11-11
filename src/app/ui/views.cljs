c2fff385e5ed3c91242680052b7b0dbb2a87fa07
amjil <amjil@protonmail.com>
2021-11-11 11:50:20 +0800

    delete text editr from home page

--- src/app/ui/views.cljs
+++ src/app/ui/views.cljs
@@ -30,7 +30,7 @@
 
 
 (when platform/android?
-  (status-bar/set-bar-style "dark-content")
+  (status-bar/set-bar-style "light-content")
   (status-bar/set-translucent true))
 
 
@@ -69,66 +69,13 @@
 
 
 
-(defn text-editor [height text]
-  (let [text-svgs (text/text-component {:width 32 :fill "gray" :color "black" :height height :font :white :font-size 24} text)
-        runs (last (last text-svgs))
-        x (reagent/atom nil)
-        y (reagent/atom nil)
-        blink-cursor? (reagent/atom true)
-        touch-state (reagent/atom 0)]
-    (reset! y (:y runs))
-    (reset! x (+ 8 (* 32 (dec (count text-svgs)))))
-    (fn []
-      [gesture/tap-gesture-handler
-       {:onHandlerStateChange #(if (gesture/tap-state-end (j/get % :nativeEvent))
-                                   (let [[ex ey] (text/cursor-location (j/get % :nativeEvent) 32 text-svgs)]
-                                     ; (js/console.log "x = " ex " y = " ey)
-                                     (reset! blink-cursor? true)
-                                     (reset! x ex)
-                                     (reset! y ey)))}
-       [gesture/pan-gesture-handler {:onGestureEvent #(let [[ex ey] (text/cursor-location (j/get % :nativeEvent) 32 text-svgs)]
-                                                        ; (js/console.log "x = " ex " y = " ey)
-                                                        (reset! blink-cursor? true)
-                                                        (reset! x ex)
-                                                        (reset! y ey))}
-        [rn/view {:style {:height "100%" :width "100%"}}
-         (when @blink-cursor?
-            [:> blinkview {"useNativeDriver" false}
-             [rn/view {:style {:position :absolute :top (or @y 0) :left (or @x 0)}}
-              [:> svg/Svg {:width 32 :height 2}
-               [:> svg/Rect {:x "0" :y "0" :width 32 :height 2 :fill "black"}]]]])
-         ; [text/flat-list-text {:width 32 :fill "gray" :color "black" :height height :font :white :font-size 24} text-svgs]]]])))
-         [rn-list/flat-list
-          {:key-fn    identity
-           :data      text-svgs
-           ; :render-fn text-list-item
-           :render-fn (partial text/flat-list-item {:width 32 :fill "gray" :color "black" :height height :font :white :font-size 24})
-           :horizontal true
-           ; :onScroll #(do
-           ;              (js/console.log "on scroll >>>>"))
-           :on-touch-start #(reset! blink-cursor? false)
-           :on-touch-move #(do
-                             (reset! blink-cursor? false)
-                             (js/console.log "on touch move"))
-           ; :onResponderMove #(js/console.log "on responder move >>>")
-           ; :onMomentumScrollBegin #(do
-           ;                           (js/console.log "onmomentum scroll start >>>"))
-           :onMomentumScrollEnd #(do
-                                    (swap! blink-cursor? not)
-                                    (js/console.log "onmomentum scroll end >>>"))
-           :onScrollBeginDrag #(do (reset! blink-cursor? false)
-                                   (js/console.log "on scroll begin >>>"))
-           :onScrollEndDrag #(do
-                               (js/console.log "on scroll end >>>"))}]]]])))
 
 (defn home []
   (let [h (reagent/atom nil)]
     (fn []
       [rn/view {:style {:height "100%" :width "100%"}
                 :on-layout #(let [height (j/get-in % [:nativeEvent :layout :height])]
-                              (reset! h height))}
-       (when @h
-         [text-editor @h font/mlongstr])])))
+                              (reset! h height))}])))
 
 (defn tabs []
   [bottom-tabs/bottom-tab
