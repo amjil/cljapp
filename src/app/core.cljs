814d15a59766d56b0395685200474242b545f078
amjil <amjil@protonmail.com>
2021-11-11 11:49:00 +0800

    move root-comp from core

--- src/app/core.cljs
+++ src/app/core.cljs
@@ -16,48 +16,16 @@
    ["react-native-smooth-blink-view" :default blinkview]
 
    [app.ui.views :as views]
+   [app.persist.realm :as realm]
    app.events
    app.subs))
-
-(defn root-comp []
-  (let [text-svgs (text/text-component {:width 32 :fill "gray" :color "black" :height 200 :font :white :font-size 24} font/mlongstr)
-        runs (last (last text-svgs))
-        y (reagent/atom (:y runs))
-        x (reagent/atom (+ 8 (* 32 (dec (count text-svgs)))))]
-    [safe-area/safe-area-view
-     [gesture/tap-gesture-handler
-      {:onHandlerStateChange #(if (gesture/tap-state-end (j/get % :nativeEvent))
-                                  (let [[ex ey] (text/cursor-location (j/get % :nativeEvent) 32 text-svgs)]
-                                    (js/console.log "x = " ex " y = " ey)))}
-      [gesture/pan-gesture-handler {:onGestureEvent #(let [[ex ey] (text/cursor-location (j/get % :nativeEvent) 32 text-svgs)]
-                                                       (js/console.log "x = " ex " y = " ey))}
-       [rn/view {:style {:flexDirection "column"
-                         :height "100%"
-                         :width "100%"}}
-                 ; :on-press #(let [[ex ey] (text/cursor-location (j/get % :nativeEvent) 32 text-svgs)]
-                 ;               (js/console.log "x = " ex " y = " ey))}
-        ; [rn/text {:style {:backgroundColor "green"}} "Hello CLojure! from CLJS "]
-        [:> blinkview {"useNativeDriver" false}
-         [rn/view {:style {:position :absolute :top (if @y @y 0) :left @x}}
-          [:> svg/Svg {:width 32 :height 2}
-           [:> svg/Rect {:x "0" :y "0" :width 32 :height 2 :fill "black"}]]]]
-        ; [rn/view {:style {:position :absolute :top 140 :left 81}}
-        ;  [:> svg/Svg {:width 45 :height 20}
-        ;   [:> svg/Circle {:cx "5" :cy "5" :r "5" :fill "black"}]
-        ;   [:> svg/Rect {:x "10" :y "4.5" :width 25 :height 2 :fill "black"}]]]
-        ; [rn/view {:style {:position :absolute :top 150 :left 81}}
-        ;  [:> svg/Svg {:width 45 :height 20}
-        ;   [:> svg/Circle {:cx "40" :cy "5" :r "5" :fill "black"}]
-        ;   [:> svg/Rect {:x "10" :y "4.5" :width 25 :height 2 :fill "black"}]]]
-
-        (text/flat-list-text {:width 32 :fill "gray" :color "black" :height 200 :font :white :font-size 24} text-svgs)]]]]))
-
-
 
 
 
 (defn init []
   (font/init)
+  (realm/Realm.copyBundledRealmFiles)
+
 
   (rn/register-comp "cljapp" views/root-stack))
 
