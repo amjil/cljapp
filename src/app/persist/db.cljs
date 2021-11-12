3fd44095d349d6bd5e82263cf1bac5720effc63e
amjil <amjil@protonmail.com>
2021-11-12 11:26:16 +0800

    android db

--- src/app/persist/db.cljs
+++ src/app/persist/db.cljs
@@ -2,6 +2,8 @@
   (:require
    [applied-science.js-interop :as j]
    [cljs-bean.core :as bean]
+   [clojure.string :as str]
+   [promesa.core :as p]
    ["react-native" :as rn]
    ["react-native-fs" :as fs]
    ["react-native-sqlite-storage" :as sqlite]))
@@ -12,31 +14,53 @@
 (def conn (atom nil))
 
 (defn open []
-  (let [platform    (j/get-in rn [:Platform :OS])
-        file-prefix (condp = platform
-                      "android" ""
-                      "ios" fs/LibraryDirectoryPath)]
-    (.then
-     (sqlite/openDatabase
-      (bean/->js
-       {:name               "cand.db"
-        :createFromLocation (str file-prefix "/LocalDatabase" "/cand.db")}))
-     #(reset! conn %))))
+  (if-not @conn
+    (let [platform    (j/get-in rn [:Platform :OS])
+          file-prefix (condp = platform
+                        "android" (str (as-> fs/DocumentDirectoryPath m
+                                         (str/split m #"/")
+                                         (drop-last m)
+                                         (concat m ["databases"])
+                                         (str/join "/" m)) "/")
+
+                        "ios" (str fs/LibraryDirectoryPath "/LocalDatabase/"))]
+      (.then
+       (sqlite/openDatabase
+        (bean/->js
+         (merge
+          {:name "cand.db"}
+
+          (condp = platform
+            "android"
+            {:location "default"}
+            "ios" {:createFromLocation (str file-prefix "cand.db")}))))
+       #(reset! conn %)))))
 
 (defn close []
   (when @conn
     (.close @conn)
-    (reset! @conn nil)))
+    (reset! conn nil)))
 
 (defn is-connected? []
   (if @conn true false))
 
 (comment
   sqlite/openDatabase
+  (open)
+  (is-connected?)
+  (close)
   conn
-  (str fs/DocumentDirectoryPath "/cand.db")
+  @conn
+  (.close @conn)
+  (reset! conn nil)
 
-  (j/call conn :executeSql "select * from a where giglgc = 'ab'")
-  (.then (.executeSql conn "select * from a where giglgc = 'ab'")
+  (.then (.executeSql @conn "select * from a where short_index = 'ab'")
   #(js/console.log "result = " (.item (.-rows (aget % 0)) 0))       )
+
+  (p/let [result (.executeSql @conn "select * from a where short_index = 'ab'")]
+    (p/then result 
+      #(do (js/console.log ">>> " (.item (.-rows (aget % 0)) 0))   
+           %)))
+  
+  
   )
--- src/app/persist/fs.cljs
+++ src/app/persist/fs.cljs
@@ -2,16 +2,27 @@
   (:require
    ["react-native-fs" :as fs]
    ["react-native" :as rn]
+   [clojure.string :as str]
    [applied-science.js-interop :as j]
    [promesa.core :as p]))
 
 (defn copy-file [filename]
   (let [platform (j/get-in rn [:Platform :OS])
-        [copy-fn src-file]
+        [copy-fn src-file des-file]
         (condp = platform
-          "android" [:copyFileAssets filename]
-          "ios"     [:copyFile (str fs/MainBundlePath "/" filename)])
-        des-file (str fs/DocumentDirectoryPath "/" "cand.db")]
+          "android"
+          [:copyFileAssets
+           filename
+           (str (as-> fs/DocumentDirectoryPath m
+                  (str/split m #"/")
+                  (drop-last m)
+                  (concat m ["databases"])
+                  (str/join "/" m)) "/cand.db")]
+
+          "ios"
+          [:copyFile
+           (str fs/MainBundlePath "/" filename)
+           (str fs/LibraryDirectoryPath "/LocalDatabase/cand.db")])]
     (.then (j/call fs copy-fn src-file des-file)
       (fn [x] (js/console.log "file copied !")))))
 
