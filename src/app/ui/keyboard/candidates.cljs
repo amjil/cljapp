a3dade4f309e6629774f9d825050bb437da33cb1
amjil <amjilt@aliyun.com>
2021-11-16 21:11:23 +0800

    fix candidate

--- src/app/ui/keyboard/candidates.cljs
+++ src/app/ui/keyboard/candidates.cljs
@@ -14,45 +14,52 @@
 
 (defn views []
   (let [candidates @(subscribe [:candidates-list])]
-    [rn/view {:style {:position "absolute"
-                      :left 0
-                      :right 0
-                    ;:top 0
-                      :bottom 10
-                      :elevation 1998
-                      :alignItems "center"
-                      :justifyContent "center"
-                      :z-index 999}}
-     [rn/view
-      {:style {:opacity 0.6
-               :backgroundColor "ghostwhite"
-               :borderRadius 5
-               :padding 10
-               :height "auto"
-               :alignItems "flex-start"
-               :justifyContent "center"
-               :maxWidth "50%"
-               :minWidth 10
-               :borderWidth 1
-               :borderColor "lightgray"
-               :flex-direction "row"}}
-      [rn-list/flat-list
-       {:key-fn    identity
-        ;; :data []
-        :data      (cond
-                     (not-empty candidates)
-                     candidates
+    (cond
+      (empty? candidates)
+      nil
 
-                     :else 
-                     [])
-        :render-fn (fn [x]
-                     [touchable/touchable-opacity {:on-press #(dispatch [:candidate-select x])}
-                      [rn/view {:style {}}
-                       [text/text-inline {:width 30
-                                          :fill "black"
-                                          :font :white
-                                          :font-size 18} (:char_word x)]]])
-        :horizontal true}]
+      :else
+      [rn/view {:style {:position "absolute"
+                        :left 0
+                        :right 0
+                      ;:top 0
+                        :bottom 10
+                        :elevation 1998
+                        :alignItems "center"
+                        :justifyContent "center"
+                        :z-index 999}}
+       [rn/view
+        {:style {:opacity 0.6
+                 :backgroundColor "ghostwhite"
+                 :borderRadius 5
+                 :padding 10
+                 ; :height "auto"
+                 ; :maxheight 100
+                 :min-height 60
+                 :alignItems "flex-start"
+                 :justifyContent "center"
+                 :maxWidth "50%"
+                 :minWidth 10
+                 :borderWidth 1
+                 :borderColor "lightgray"
+                 :flex-direction "row"}}
+        [rn-list/flat-list
+         {:key-fn    identity
+          ;; :data []
+          :data      (cond
+                       (not-empty candidates)
+                       candidates
+
+                       :else
+                       [])
+          :render-fn (fn [x]
+                       [touchable/touchable-opacity {:on-press #(dispatch [:candidate-select x])}
+                        [rn/view {:style {}}
+                         [text/text-inline {:width 30
+                                            :fill "black"
+                                            :font :white
+                                            :font-size 18} (:char_word x)]]])
+          :horizontal true}]]])))
     ;; [text/text-inline {:width 30
     ;;                    :fill "black"
     ;;                    :font :white
@@ -68,4 +75,3 @@
     ;;                    :font :white
     ;;                    :font-size 18}
     ;;  "ᠭᠤᠷᠪᠠ"]
-      ]]))
