a3c6b946853e7f7db0bb91c8ed7f2b6765ad01de
amjil <amjil@protonmail.com>
2021-11-19 15:06:33 +0800

    delete anchor-point

--- src/app/core.cljs
+++ src/app/core.cljs
@@ -5,7 +5,6 @@
    [app.ui.views :as views]
    [honey.sql :as hsql]
    ["react-native-sqlite-storage" :as sqlite]
-   ["react-native-anchor-point" :refer [withAnchorPoint]]
    ["react-native-text-size" :as rnsize]
    app.events
    app.subs))
--- src/app/ui/home/views.cljs
+++ src/app/ui/home/views.cljs
@@ -5,63 +5,48 @@
    [cljs-bean.core :as bean]
    [applied-science.js-interop :as j]
    [steroid.rn.core :as rn]
-   [promesa.core :as p]
-   ["react-native-anchor-point" :refer [withAnchorPoint]]))
+   [promesa.core :as p]))
 
 
 (defn home []
   (let [h (reagent/atom nil)
-        height 20
-        width 300
+        height 300
+        width 20
         offset (- (/ height 2) (/ width 2))]
     (fn []
       [ui/safe-area-consumer
-      ;;  [rn/view {:style {:width "100%" :height "100%" :alignItems "flex-start" :justifyContent "flex-start"}}
-      ;; has some transform issue
-      ;;  [rn/view {:style {:width height :height width :backgroundColor "red"}}
-      ;;   [rn/text {:style (merge (bean/->clj (withAnchorPoint (bean/->js {:transform [{:perspective 400}
-      ;;                                                                         {:rotate "90deg"}]})
-      ;;                                                 (bean/->js {:x -0 :y 1})
-      ;;                                                 (bean/->js {:width width, :height height})))
-      ;;                           {:height height :width width :backgroundColor "yellow"}
-      ;;   )}
-      ;;    "hello world!"]
-      ;;  ]
       ;; OK this style
-       [rn/view {:style {:width  height :height width :backgroundColor "red"}}
-             [rn/text {:style {:width width :height height
+      ;;  [rn/view {:style {:width  width :height height :backgroundColor "red"}}
+      ;;        [rn/text {:style {:width height :height width
+      ;;                          :backgroundColor "yellow"
+      ;;                          :transform [{:rotate "90deg"}
+      ;;                                      {:translateX offset}
+      ;;                                      {:translateY offset}
+      ;;                                      ]}
+      ;;                 ;;  :selectable true
+      ;;                  }
+      ;;          "hello world!" ]
+      ;;   ]
+       [rn/view {:style {:width "100%"
+                         :flex 2}
+                 :on-layout #(let [height (j/get-in % [:nativeEvent :layout :height])]
+                               (reset! h height))}
+        (when @h
+          (let [offset (- (/ @h 2) (/ 20 2))]
+            [rn/view {:style {:height @h :width width :backgroundColor "red"}}
+             [rn/text {:style {:width @h :height width
                                :backgroundColor "yellow"
                                :transform [{:rotate "90deg"}
-                                           {:translateX (- offset)}
-                                           {:translateY (- offset)}
-                                           ]}}
-               "hello world!" ]
-        ]
-      ;;  [rn/view {:style {:width "100%"
-      ;;                    :flex 2}
-      ;;            :on-layout #(let [height (j/get-in % [:nativeEvent :layout :height])]
-      ;;                          (reset! h height))}
-      ;;   (when @h
-      ;;     (let [offset (- (/ @h 2) (/ 20 2))]
-      ;;       [rn/view {:style {:height @h :width 20}}
-      ;;        [rn/text {:style {:width @h :height 20 :transform [{:rotate "90deg"}
-      ;;                                                           {:translateX (- offset)}
-      ;;                                                           {:translateY offset}]}}
-      ;;          "abc"]
-      ;;        ]))]
+                                           {:translateX offset}
+                                           {:translateY offset}]}}
+               "hello world!"]
+             ]))]
        ])))
          
 
 
 (comment
-  (require '["react-native-anchor-point" :refer [withAnchorPoint]])
-  withAnchorPoint
 
-  (bean/->clj
-   (withAnchorPoint (bean/->js {:transform [{:perspective 400}
-                                            {:rotate 90}]})
-                    (bean/->js {:x 0.5 :y 0})
-                    (bean/->js {:width 20, :height 80})))
   (require '[app.font.base :as font])
   (require '["react-native-text-size" :default rnsize])
 
