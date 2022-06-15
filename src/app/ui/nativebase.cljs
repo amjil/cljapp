e92ca26e6052a6425711bfe2d7a8d320d4ecf9d7
amjil <amjil@protonmail.com>
2022-06-15 11:03:13 +0800

    checkbox

--- src/app/ui/nativebase.cljs
+++ src/app/ui/nativebase.cljs
@@ -28,6 +28,7 @@
                           Text
                           Button Button.Group
                           Input
+                          Checkbox
                           Link
                           CloseIcon
                           IconButton
@@ -77,6 +78,7 @@
 
 (def text (reagent/adapt-react-class Text))
 (def input (reagent/adapt-react-class Input))
+(def checkbox (reagent/adapt-react-class Checkbox))
 
 (def button (reagent/adapt-react-class Button))
 (def button-group (reagent/adapt-react-class Button.Group))
